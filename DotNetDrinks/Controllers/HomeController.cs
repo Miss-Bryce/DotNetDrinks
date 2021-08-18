9caf2f755f29138547b768a2613445d0f9547f32
J. Bryce <53979775+Miss-Bryce@users.noreply.github.com>
2021-08-18 11:13:39 -0400

    Initial Changes

--- DotNetDrinks/Controllers/HomeController.cs
+++ DotNetDrinks/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace DotNetDrinks.Controllers
             return View("Privacy");
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
